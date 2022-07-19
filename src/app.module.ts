28cea719bb05a1447b1ece849bd6c32c853b63da
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-19 07:37:37 +0100

    add-orgaization-folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { UsersModule } from './users/users.module';
 import { AuthenticationService } from './authentication/authentication.service';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { OrganizationsService } from './organizations/organizations.service';
+import { OrganizationsController } from './organizations/organizations.controller';
+import { OrganizationsModule } from './organizations/organizations.module';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
-  controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  imports: [UsersModule, AuthenticationModule, OrganizationsModule],
+  controllers: [AuthenticationController, OrganizationsController],
+  providers: [AuthenticationService, OrganizationsService],
 })
 export class AppModule {}
