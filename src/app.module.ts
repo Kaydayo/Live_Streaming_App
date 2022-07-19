97896b8a8d80a4cc5e1cc472791cd36144b59db1
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-19 07:41:15 +0100

    added-Notification-payment

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,16 @@ import { OrganizationsModule } from './organizations/organizations.module';
 import { CoursesService } from './courses/courses.service';
 import { CoursesController } from './courses/courses.controller';
 import { CoursesModule } from './courses/courses.module';
+import { NotificationService } from './notification/notification.service';
+import { PaymentService } from './payment/payment.service';
+import { NotificationController } from './notification/notification.controller';
+import { PaymentController } from './payment/payment.controller';
+import { NotificationModule } from './notification/notification.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule, OrganizationsModule, CoursesModule],
-  controllers: [AuthenticationController, OrganizationsController, CoursesController],
-  providers: [AuthenticationService, OrganizationsService, CoursesService],
+  imports: [UsersModule, AuthenticationModule, OrganizationsModule, CoursesModule, NotificationModule, PaymentModule],
+  controllers: [AuthenticationController, OrganizationsController, CoursesController, NotificationController, PaymentController],
+  providers: [AuthenticationService, OrganizationsService, CoursesService, NotificationService, PaymentService],
 })
 export class AppModule {}
