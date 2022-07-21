4866523c73b87f1f3dc88d0cfff7e0b3d219b94a
Mujeeb Adebowale <olukayodedayo100@gmail.com>
2022-07-21 09:05:24 +0100

    model-schema

---
+++ src/Users/users.schema.ts
@@ -0,0 +1,57 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { ROLES } from 'src/utils/enums';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop({required: true})
+    username: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({required: true})
+    email: string;
+
+    @Prop({required: true})
+    firstName: string;
+
+    @Prop({required: true})
+    lastName: string;
+
+    @Prop({type:[String], default:[ROLES.USER], enum:ROLES})
+    role: string[];
+
+    @Prop({required: true})
+    phoneNumber: string;
+
+    @Prop()
+    address: string;
+
+    @Prop({default: false})
+    isAdmin: boolean;
+
+    @Prop({default:false})
+    isActive: boolean;
+
+    @Prop({default: false})
+    isDeleted: boolean;
+
+    @Prop({default: false})
+    isLicense: boolean;
+
+    @Prop({default: false})
+    organization: string;
+
+    @Prop()
+    licenseExpiryDate: Date;
+    
+  
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+
+
